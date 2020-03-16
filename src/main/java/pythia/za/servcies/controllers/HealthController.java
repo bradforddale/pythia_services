51fd36e00edb12635ca884d6981ad30fb1694e56
bradforddale <bradford.dale1@gmail.com>
2020-03-16 14:23:06 +0200

    chore: allow cross origin on endpoints for initial testing

--- src/main/java/pythia/za/servcies/controllers/HealthController.java
+++ src/main/java/pythia/za/servcies/controllers/HealthController.java
@@ -3,6 +3,7 @@ package pythia.za.servcies.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +17,13 @@ public class HealthController {
     private HealthServices healthServices;
 
     @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
+    @CrossOrigin
     public ResponseEntity<SystemHealth> getHealth() {
         return new ResponseEntity<SystemHealth>(healthServices.getSystemHealth(), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/health", method = RequestMethod.GET, produces = "application/json")
+    @CrossOrigin
     public ResponseEntity getAllHealth() {
         return new ResponseEntity(healthServices.getAllSystemHealth(), HttpStatus.OK);
     }
